d62f1c5acb5096671887e983b3cc412b6737a51e
a17364 <a17364@d1-08-06.dped.local>
2023-02-10 11:36:10 +0000

    Alteração requisitada

--- src/main/java/turma11o/m17b_projetocolab_fichatrab3/M17B_projetoColab_FichaTrab3.java
+++ src/main/java/turma11o/m17b_projetocolab_fichatrab3/M17B_projetoColab_FichaTrab3.java
@@ -53,7 +53,10 @@ public class M17B_projetoColab_FichaTrab3 {
         System.out.println(poligonos[5]);
         
         poligonos[10] = new Retangulo("a22235", 7, 8);
-        System.out.println(poligonos[5]);
+        System.out.println(poligonos[10]);
+        
+        poligonos[16] = new Retangulo("a17364", 4, 9);
+        System.out.println(poligonos[16]);
         
         
         poligonos[7] = new Retangulo("f133",9,10);
